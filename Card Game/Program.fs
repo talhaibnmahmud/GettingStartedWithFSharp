12d5801459b6484b8bafc58db03e4e3b607d5573
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-19 02:43:41 +0600

    Card Game

---
+++ Card Game/Program.fs
@@ -0,0 +1,26 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let cardDescription (card: int): string = 
+    let cardNumber = card % 13
+    if cardNumber = 1 then "Ace"
+    elif cardNumber = 11 then "Jack"
+    elif cardNumber = 12 then "Queen"
+    elif cardNumber = 0 then "King"
+    else string cardNumber
+
+
+let suit (number: int) : string = 
+    let suitNumber: int = number / 13
+    if suitNumber = 0 then "Hearts"
+    elif suitNumber = 1 then "Spades"
+    elif suitNumber = 2 then "Diamonds"
+    else "Clubs"
+
+
+[<EntryPoint>]
+let main _argv = 
+    let cards = [ 1; 10; 2; 34 ]
+    for card in cards do
+        printfn "%s of %s" (cardDescription(card)) (suit(card))
+
+    0
