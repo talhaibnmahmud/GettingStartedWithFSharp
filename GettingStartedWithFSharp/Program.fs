ac24de6fc5b6f6a94cd3ad0a3b0ee099f8953956
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-19 01:11:49 +0600

    Learnt Variables, Data storing & Printing

--- GettingStartedWithFSharp/Program.fs
+++ GettingStartedWithFSharp/Program.fs
@@ -1,4 +1,23 @@
-﻿
+﻿// For more information see https://aka.ms/fsharp-console-apps
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+// Bind a value to a variable (Immutable By Default)
+let name = "Talha"
+
+// Not Allowed
+// name <- "John Doe"
+
+// Making the varibale mutable with the "mutable" keyword
+let mutable age = 23
+age <- 24               // Allowed
+
+// Variable types
+let university = "KUET"     // String
+let moneyLeft = 25.75       // Float
+let isDesert = true         // Bool
+
+// Printing to the console
+printfn "Hello, I'm learning F#"
+
+// Foramtting
+printfn $"Name: {name}, University: {university}"
+printfn "I have %f Taka left" moneyLeft
