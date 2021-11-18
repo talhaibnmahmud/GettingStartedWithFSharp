782f0cb2a1cb126895e6632b5b8473b09efe3d7d
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-19 01:45:34 +0600

    Building a Calculator

---
+++ Calculator/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+
+[<EntryPoint>]                                      // Indicating the application entry point explicitly
+let main argv = 
+    printfn "Welcome to the calculator Program"
+
+    printf "Type the first number: "
+    let firstNumber = Console.ReadLine()
+    printf "Type the second number: "
+    let secondNumber = Console.ReadLine()
+
+    printfn "First: %s, Second: %s" firstNumber secondNumber
+
+    let sum = (int firstNumber) + (int secondNumber)
+    printfn "The sum is %i" sum
+    0                                               // Returns an integer exit code
