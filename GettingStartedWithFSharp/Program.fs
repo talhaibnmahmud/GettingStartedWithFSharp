59b602fe34a90df5bcd91900238c222ae045ae15
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-19 02:05:38 +0600

    Branching

--- GettingStartedWithFSharp/Program.fs
+++ GettingStartedWithFSharp/Program.fs
@@ -23,7 +23,7 @@ printfn $"Name: {name}, University: {university}"
 printfn "I have %f Taka left" moneyLeft
 
 // Read from the Console
-System.Console.Write "Type a value: "
+System.Console.Write "Type something: "
 let str = System.Console.ReadLine()
 printfn "You typed %s" str
 
@@ -35,3 +35,28 @@ printfn "Number is: %i" numberedVersion
 let second = "57"
 numberedVersion <- int second
 printfn "Number is: %i" numberedVersion                     // Prefered
+
+
+// Branching
+printf "Enter a number: "
+let seniorityCheck = System.Console.ReadLine()
+if (int seniorityCheck) > 65
+then printfn "You're a senior citizen"
+else printfn "Citizen"
+
+let messeage = if (int seniorityCheck) > 65 then "Senior Citizen" else "Citizen"
+printfn "%s" messeage
+
+let cardValue = 1
+let cardDescription = if cardValue = 1 then "Ace" elif cardValue = 14 then "Ace" else "A Card"
+printfn "%s" cardDescription
+
+// Branching Exercise
+let cardNo = 12
+let cardName = 
+    if cardNo = 1 || cardNo = 14 then "Ace"
+    elif cardNo = 11 then "Jack"
+    elif cardNo = 12 then "Queen"
+    elif cardNo = 13 then "King"
+    else string cardNo
+printfn "%s" cardName
