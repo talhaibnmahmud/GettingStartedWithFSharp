d2b63cda3f9b93dd550c0e9c2fca17c1ffaaf2fd
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-11-19 01:44:49 +0600

    Reading From Console, Convert between data types

--- GettingStartedWithFSharp/Program.fs
+++ GettingStartedWithFSharp/Program.fs
@@ -8,12 +8,12 @@ let name = "Talha"
 
 // Making the varibale mutable with the "mutable" keyword
 let mutable age = 23
-age <- 24               // Allowed
+age <- 24                                                   // Allowed
 
 // Variable types
-let university = "KUET"     // String
-let moneyLeft = 25.75       // Float
-let isDesert = true         // Bool
+let university = "KUET"                                     // String
+let moneyLeft = 25.75                                       // Float
+let isDesert = true                                         // Bool
 
 // Printing to the console
 printfn "Hello, I'm learning F#"
@@ -21,3 +21,17 @@ printfn "Hello, I'm learning F#"
 // Foramtting
 printfn $"Name: {name}, University: {university}"
 printfn "I have %f Taka left" moneyLeft
+
+// Read from the Console
+System.Console.Write "Type a value: "
+let str = System.Console.ReadLine()
+printfn "You typed %s" str
+
+// Convert between data types
+let first = "57"
+let mutable numberedVersion = System.Int32.Parse first
+printfn "Number is: %i" numberedVersion
+
+let second = "57"
+numberedVersion <- int second
+printfn "Number is: %i" numberedVersion                     // Prefered
